c46ac31f48bacda1782c994312df9f9c6df0302a
Alina Pohl <alinaepohl@gmail.com>
2022-11-07 17:57:28 +0100

    Add functions to buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 
 const board = document.querySelector(".board");
-const squares = document.getElementsByClassName(".square");
 const slider = document.querySelector(".slider-container input");
 const sizeXsize = document.querySelector(".slider-container p");
 const rainbowBtn = document.querySelector(".rainbow");
+// const gridBtn = document.querySelector(".grid");
+const customPenColor = document.querySelector("input.pen-color");
+const customBgcColor = document.querySelector("input.bgc-color");
 let size;
 let mouseDown = false;
 const sliderValues = [];
@@ -38,7 +40,7 @@ const sketchingMechanism = (e) => {
     e.target.className == "square" &&
     rainbowBtn.textContent === "rainbow"
   ) {
-    e.target.style.backgroundColor = `var(--clr-theme-500)`;
+    customPen(e);
   } else if (
     mouseDown &&
     e.target.className == "square" &&
@@ -46,6 +48,10 @@ const sketchingMechanism = (e) => {
   ) {
     rainbowPen(e);
   }
+};
+const backgroundMechanism = () => {
+  let pickedBgcColor = customBgcColor.value;
+  board.style.backgroundColor = pickedBgcColor;
 };
 const rainbowBtnText = () => {
   if (rainbowBtn.textContent === "rainbow") {
@@ -60,10 +66,16 @@ const rainbowPen = (e) => {
   let b = Math.floor(Math.random() * 255);
   e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
 };
+const customPen = (e) => {
+  let pickedPenColor = customPenColor.value;
+  e.target.style.backgroundColor = `${pickedPenColor}`;
+};
+
 document.addEventListener("DOMContentLoaded", getGridSize);
 slider.addEventListener("input", getGridSize);
 board.addEventListener("mousedown", allowDrawing);
 board.addEventListener("mouseup", disallowDrawing);
 board.addEventListener("mouseleave", disallowDrawing);
 board.addEventListener("mouseover", sketchingMechanism);
+customBgcColor.addEventListener("input", backgroundMechanism);
 rainbowBtn.addEventListener("click", rainbowBtnText);
